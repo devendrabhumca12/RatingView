9ab09386a974a09bd7a519a3a3c1ce1eceff75f6
Devendra Kumar <devendra.kumar@ukg.com>
2023-04-24 18:14:47 +0530

    Initial Commit

---
+++ RatingView/RatingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingViewApp.swift
+//  RatingView
+//
+//  Created by Devendra Kumar on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RatingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RatingViewUITests/RatingViewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RatingViewUITestsLaunchTests.swift
+//  RatingViewUITests
+//
+//  Created by Devendra Kumar on 24/04/23.
+//
+
+import XCTest
+
+final class RatingViewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
